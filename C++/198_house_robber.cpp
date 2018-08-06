506c217a5660684d2daeb295f607f1f93a3e7530
Yufeng Xiong <kopxiong1989@gmail.com>
2018-08-06 15:17:32 +0200

    198 C++ commit

---
+++ C++/198_house_robber.cpp
@@ -0,0 +1,40 @@
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int rob(std::vector<int>& nums) {
+        // base case
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return std::max(nums[0], nums[1]);
+        }
+        int len = nums.size();
+        int dp[len];
+        dp[0] = nums[0];
+        dp[1] = std::max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); ++i) {
+            dp[i] = std::max(nums[i] + dp[i-2], dp[i-1]);
+        }
+
+        return dp[nums.size()-1];
+    }
+};
+
+
+int main() {
+    Solution Test;
+    std::vector<int> nums = {1, 2, 3, 1};
+
+    std::cout << "maximum robbed money: " << Test.rob(nums) << std::endl;
+}
